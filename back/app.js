b12675d7ed4296e0113571ad4ddd124b72116e8d
o2caledonie <krn.sigura@yahoo.com>
2021-06-01 17:06:18 +0200

    get all users useless to delete

--- back/app.js
+++ back/app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://o2caledonie:WjgjWxuzzdxWdJ42@cluster0.7p67r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
@@ -9,23 +23,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
