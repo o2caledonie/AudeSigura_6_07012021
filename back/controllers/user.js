738700a6534d66bb288b8fb2dc670f439c316b94
o2caledonie <krn.sigura@yahoo.com>
2021-06-02 13:23:16 +0200

    get all users commented

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -42,8 +42,8 @@ exports.login = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
-exports.getUsers = (req, res, next) => {
-    User.find()
-        .then(users => res.status(200).json({ users }))
-        .catch(error => res.status(400).json({ error }));
-}
+// exports.getUsers = (req, res, next) => {
+//     User.find()
+//         .then(users => res.status(200).json({ users }))
+//         .catch(error => res.status(400).json({ error }));
+// }
--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const saucesCtrl = require('../controllers/sauces');
 
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
@@ -11,8 +11,6 @@ router.delete('/:id', auth, saucesCtrl.deleteSauce);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.get('/', auth, saucesCtrl.getAllSauces);
 
-// router.get('/:id/like', (req, res, next) => {
-//     Sauce.
-// })
+// router.post('/:id/like', auth, sauceCtrl.rateSauce);
 
 module.exports = router;
